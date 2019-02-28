1f3f3e3e07dc44405ec172f84b9ac7511e2e1225
SzilardZ <szilard.zr@gmail.com>
2019-02-28 13:20:45 +0100

    successAlert component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent} from './warning_alert/warningAlert.component';
+import {SuccessAlertComponent} from './success_alert/successAlert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
