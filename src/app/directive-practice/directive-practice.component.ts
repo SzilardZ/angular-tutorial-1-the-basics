0ecf80b75b31cad7b03fc252b85002dbd7b7cf5b
SzilardZ <szilard.zr@gmail.com>
2019-03-04 22:25:48 +0100

    directives assignment: add new directive-practice component

---
+++ src/app/directive-practice/directive-practice.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-directive-practice',
+  templateUrl: './directive-practice.component.html',
+  styleUrls: ['./directive-practice.component.css']
+})
+
+export class DirectivePracticeComponent {
+}
